68bb6ad6f80edf7ea1c49685b964607d9278b2a2
koop4 <dilillo.roberto@gmail.com>
2017-02-11 11:25:20 -0600

    add webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    //__dirname refers to the directory where this webpack.config.js lives
+  context: path.resolve(__dirname, './src'),
+  entry: {
+    app: './builder.js',
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'site.bundle.js',
+  },
+};
