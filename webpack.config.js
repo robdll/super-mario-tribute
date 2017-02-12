bfe121af325a0cb6ff23a5c1efe28c2d54ea53b2
koop4 <dilillo.roberto@gmail.com>
2017-02-11 20:06:23 -0600

    add babel

--- webpack.config.js
+++ webpack.config.js
@@ -4,15 +4,27 @@ const webpack = require('webpack');
 module.exports = {
     //__dirname refers to the directory where this webpack.config.js lives
   context: path.resolve(__dirname, './src'),
-  entry: {
-    app: './builder.js',
+  entry: { 
+    app: './builder.js' 
   },
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: 'site.bundle.js',
     publicPath: '/assets',
   },
-  devServer: {
-    contentBase: path.resolve(__dirname, './src'),  // New
+  devServer: { 
+    contentBase: path.resolve(__dirname, './src') 
   },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  }
 };
