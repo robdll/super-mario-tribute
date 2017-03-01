60f16a1d7c9f9f6521f9e40e827b33ec633d574b
koop4 <dilillo.roberto@gmail.com>
2017-02-28 22:20:43 -0600

    edit import and update bundle

--- src/builder.js
+++ src/builder.js
@@ -1,8 +1,8 @@
 import "./css/style.css";
 import "./resources/font/prstart.ttf";
-import "./resources/img/background_475.jpg";
-import "./resources/img/background_900.jpg";
-import "./resources/img/background_1920.jpg";
+import "./resources/img/background_475.png";
+import "./resources/img/background_900.png";
+import "./resources/img/background_1920.png";
 import "./resources/img/center_pipe_475.png";
 import "./resources/img/center_pipe_900.png";
 import "./resources/img/center_pipe_1920.png";
